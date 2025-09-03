93bbcb9cac690ae9a0c7874ec5ffd38a95e7d179
KiamMota <somentekiam@gmail.com>
2025-09-02 21:28:29 -0300

    add : windows dir

--- include/internal/base.h
+++ include/internal/base.h
@@ -2,7 +2,8 @@
 #define _BASE_H_
 
 int dir_make(const char *__restrict name);
-int dir_del(const char *__restrict name);
+int dir_exists(const char*__restrict name);
 int dir_move(const char *__restrict name, const char *__restrict path);
+int dir_del(const char *__restrict name);
 
 #endif
--- include/internal/win.h
+++ include/internal/win.h
@@ -3,7 +3,29 @@
 #ifndef _WINMKDIR_H_
 #define _WINMKDIR_H_
 
-#include "windows.h"
+#include "base.h"
+#include <Windows.h>
+
+int dir_make(const char *path) {
+    return CreateDirectoryA(path, NULL) ? 0 : -1;
+}
+
+int dir_remove(const char *path) {
+    return RemoveDirectoryA(path) ? 0 : -1;
+}
+
+int dir_exists(const char *path) {
+    DWORD dwAttrib = GetFileAttributesA(path);
+    return (dwAttrib != INVALID_FILE_ATTRIBUTES && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY)) ? 1 : 0;
+}
+
+int dir_move(const char *oldpath, const char *newpath) {
+    return MoveFileA(oldpath, newpath) ? 0 : -1;
+}
+
+int dir_del(const char *path) {
+    return RemoveDirectoryA(path) ? 0 : -1;
+}
 
 #endif
 
